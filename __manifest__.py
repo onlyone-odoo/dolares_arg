8d518616b596e6f885b8401fddb936dcea07b3d9
onlyone-odoo <matiasb@onlyone.com.ar>
2025-10-21 21:19:49 -0300

    fix cron data

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,7 @@
     "website": "https://onlyone.odoo.com/",
     "license": "AGPL-3",
     "category": "Accounting",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.1",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
